d5b2431b277e1ae8dbab6d0b41a3aaded108ba46
Dae Kyoung Lim <daekyoung.lim@gmail.com>
2014-11-23 00:31:49 +0900

    make init.py

---
+++ photolog/model/user.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationships
+
+from photolog.model import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50),unique=True)
+    email = Column(String(50),unique=False)
+    password = Column(String(55),unique=False)
+    photos = relationships('Photo',
+                           backref='user',
+                           cascade='all, delete, delete-orphan')
+
+def __init__(self,name,email,password):
+    self.username = name
+    self.email = email
+    self.password = password
+
+def __repr__(self):
+    return '<User %r %r>' % (self.username, self.email)
